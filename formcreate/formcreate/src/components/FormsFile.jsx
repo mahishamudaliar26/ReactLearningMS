c8ff7340995e572d01933139694e304465a15da4
Mahisha Mudaliar <mahisha.mudaliar@atqor.com>
2024-03-18 19:07:03 +0530

    Input data in array

--- formcreate/formcreate/src/components/FormsFile.jsx
+++ formcreate/formcreate/src/components/FormsFile.jsx
@@ -2,25 +2,51 @@ import React, { useState } from "react";
 import "../App.css";
 
 function FormsFile() {
-  const [getvalue, setValue] = useState("");
+  const [values, setValues] = useState({ firstName: "", lastName: "" });
+  const [submittedValues, setSubmittedValues] = useState([]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+    // setSubmittedValues([]);
+  };
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    setSubmittedValues((prevSubmittedValues) => [
+      ...prevSubmittedValues,
+      values,
+    ]);
+    setValues({ firstName: "", lastName: "" });
+  };
+
+  console.log(submittedValues);
 
   return (
     <>
       <h4 style={{ paddingLeft: "50px", fontSize: "25px" }}>First Name: </h4>
       <input
         type="text"
+        name="firstName"
+        onChange={handleChange}
         style={{ marginLeft: "50px", height: "30px", width: "500px" }}
       ></input>
       <h4 style={{ paddingLeft: "50px", fontSize: "25px" }}>Last Name: </h4>
       <input
         type="text"
+        name="lastName"
+        onChange={handleChange}
         style={{ marginLeft: "50px", height: "30px", width: "500px" }}
       ></input>
       <br></br>
       <br></br>
-      <button style={{ marginLeft: "50px" }}>Submit</button>
+      <button
+        onClick={handleSubmit}
+        style={{ marginLeft: "50px", backgroundColor: "#FFBF00" }}
+      >
+        Submit
+      </button>
     </>
   );
 }
